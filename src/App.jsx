49284d82926995372d2f96670454f01356ffa9e4
abhistormwastaken <abhistorm409@gmail.com>
2024-02-09 22:21:11 +0530

    checking whether atom-family makes mukltiple atoms when same id is called multiple times

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useSetRecoilState, useRecoilValue } from 'recoil';
 import { todosAtomFamily } from './atoms';
+import { useEffect } from 'react';
 
 
 // simple:
 // you don't know kitne atoms banane hain, use atom-family
 function App() {
+  // lets check something
+
   return <RecoilRoot>
+    <UpdateYourTodo/>
     <Todo id={1}/>
     <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
+    <Todo id={2} />
   </RecoilRoot>
+}
+
+function UpdateYourTodo() {
+  // get todo with id 2
+  const updateTodo = useSetRecoilState(todosAtomFamily(2));
+  // update it after 3 seconds to check if all with id 2 update or not, this would show that only one atom with id 2 is created and all are using the same atom
+  useEffect(()=>{
+    setTimeout(()=>{
+      updateTodo({
+        id: 2,
+        title: "Updated Title",
+        description: "Updated Description"
+      })
+    }, 3000)
+  }, [])
+
+  return <div></div>
 }
 
 // what do we do if we need to make atoms dynamically based on id? atom family coems in if we don't know how many atoms we need to create and dont't want to use an array.
 // atom family is a function that returns an atom based on the parameter passed to it.
 function Todo({id}) {
-  const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+  const todo = useRecoilValue(todosAtomFamily(id));
 
   return (
     <>
--- src/atoms.js
+++ src/atoms.js
@@ -6,6 +6,8 @@ export const todosAtomFamily = atomFamily({
   default: id => {
     // here we have access to the id passed to the atom family and can return the todo with that id
     // TODOS is hardcoded here but in a real app this would be a call to a backend or something
-    return TODOS.find(x => x.id === id)
+    return TODOS.find(todo => todo.id === id)
   },
 });
+
+// atomFamily is a function that returns an atom, this is done dynamically based on the parameter passed to it.
